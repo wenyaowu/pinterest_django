485b01b4169c15e13f6888154bda2a47ea3e3f4e
wenyaowu <evanwu0225@gmail.com>
2015-05-12 14:13:08 -0700

    Close Pin function (masonry)

--- pinterest_django/static/js/pinterest-django.js
+++ pinterest_django/static/js/pinterest-django.js
@@ -7,3 +7,13 @@ $('button.like-button').click(function(){
             $('button.like-button[data-pinid="'+pinid+'"]').prop("disabled",true);
         });
     });
+
+$('button.close-button').click(function(){
+   var pinid;
+   var container = document.querySelector('#pin-masonry-container');
+   var msnry = new Masonry( container );
+   pinid = $(this).attr("data-pinid");
+    $('div.pin-container[data-pinid="'+pinid+'"]').remove();
+    msnry.layout()
+});
+
