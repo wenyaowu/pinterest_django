72c9d477fdd355edc76ab381788b5d6b9f681556
wenyaowu <evanwu0225@gmail.com>
2015-05-15 00:42:26 -0700

    Change production setting

--- pinterest_django/pinterest_django/production_settings.py
+++ pinterest_django/pinterest_django/production_settings.py
@@ -16,6 +16,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Turn off DEBUG mode
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
