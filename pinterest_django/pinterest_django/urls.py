35246e9377181bf178d95a2591ae7c6d3601d297
wenyaowu <evanwu0225@gmail.com>
2015-05-16 23:26:23 -0700

    Change deploy Proc

--- pinterest_django/pinterest_django/urls.py
+++ pinterest_django/pinterest_django/urls.py
@@ -14,9 +14,6 @@ urlpatterns = patterns('',
 
 )
 
-urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
 
 if settings.DEBUG:
     urlpatterns += patterns(
